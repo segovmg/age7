69b43b71f7f1b674bfbc8a526655ef6e8271e315
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2021-07-13 18:11:18 -0300

    Reseta alterações de arquivos que não deveriam ser modificados

--- scripts/exploratory-analysis.R
+++ scripts/exploratory-analysis.R
@@ -2,16 +2,14 @@ library(dplyr); library(data.table)
 
 source("scripts/utils.R")
 
-resource_name = "dm-subfuncao-desp" # ajustar
+resource_name = "ft_orcamento_cred_alt" # ajustar
 resource <- fread(glue::glue('data/{gsub("_", "-", resource_name)}.csv.gz'), dec = ",")
 
 glimpse(resource)
 
 ## field constraints
 
-field <- "id_subfuncao" 
-
-# ajustar
+field <- "nr_documento" # ajustar
 
 head(resource[, ..field])
 describe_unique(resource[[field]])
