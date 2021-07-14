32db977e222438d402591ce54ef36ee82efaaf05
kesiabomfa <kesiabomfa@users.noreply.github.com>
2021-07-14 11:15:52 -0300

    [#11] atualiza o scripts exploratory-analysis

--- scripts/exploratory-analysis.R
+++ scripts/exploratory-analysis.R
@@ -2,14 +2,14 @@ library(dplyr); library(data.table)
 
 source("scripts/utils.R")
 
-resource_name = "dm_elemento_desp" # ajustar
+resource_name = "dm_item_desp" # ajustar
 resource <- fread(glue::glue('data/{gsub("_", "-", resource_name)}.csv.gz'), dec = ",")
 
 glimpse(resource)
 
 ## field constraints
 
-field <- "cd_elemento" # ajustar
+field <- "cd_item" # ajustar
 
 head(resource[, ..field])
 describe_unique(resource[[field]])
