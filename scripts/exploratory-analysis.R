a6cb44bbfcf89488dd11c9724ac677587a96560e
kesiabomfa <kesiabomfa@users.noreply.github.com>
2021-07-14 11:14:17 -0300

    [#11] inclui alterações no conjunto de dados item de despesa

--- scripts/exploratory-analysis.R
+++ scripts/exploratory-analysis.R
@@ -2,14 +2,14 @@ library(dplyr); library(data.table)
 
 source("scripts/utils.R")
 
-resource_name = "ft_orcamento_cred_alt" # ajustar
+resource_name = "dm_elemento_desp" # ajustar
 resource <- fread(glue::glue('data/{gsub("_", "-", resource_name)}.csv.gz'), dec = ",")
 
 glimpse(resource)
 
 ## field constraints
 
-field <- "nr_documento" # ajustar
+field <- "cd_elemento" # ajustar
 
 head(resource[, ..field])
 describe_unique(resource[[field]])
