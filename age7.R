9a31c8c78cc3a2e4cb0d7091f49a102c08285f5c
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2021-07-22 12:05:04 -0300

    Simplifica script de status

--- age7.R
+++ age7.R
@@ -1,18 +1,9 @@
-library(yaml); library(purrr); library(data.table)
+library(yaml); library(purrr); library(data.table); library(jsonlite)
 
 # gh pr list --assignee @me
 # gh pr list --assignee kesiabomfa
 # status: Todo; Open; Assigned; Merge; Merged
 #======================================================================
 
-age7 <- read_yaml("age7.yaml")
-
-status <- data.table(resource = names(age7$resources), 
-                     status = map_chr(age7$resources, "status"))
-
-status[, .N, status]
-
-status[status == 'Todo']
-
-map(age7, "notes") |> unlist()
-
+age7_status <- read_yaml("age7.yaml")
+age7_dp <- read_json("datapackage.json")
