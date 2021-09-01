2a8dcb7f66f642af349e8808d0f25e2ad44fb347
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2021-09-01 18:31:00 -0300

    Rewrite scripts/validate-resource.py to use frictionless.py

--- Makefile
+++ Makefile
@@ -16,8 +16,8 @@ help:
 validate: clean $(VALIDATION_REPORTS) ## Valida recursos que sofreram modificação
 
 validate_data: ## Valida arquivo (usage: make validate_data resource=resource_name)
-	@echo "Validando recurso $(resource_name)"
-	@python scripts/validate-resource.py $(resource_name)
+	@echo "Validando recurso $(resource)"
+	@python scripts/validate-resource.py $(resource) | jq . 2>&1 | tee logs/$(resource).json
 
 describe: ## Extrai dados e metadados do banco de dados Oracle (make describe resource=resource_name)
 	Rscript scripts/describe_resource.R $(resource)
--- scripts/validate-resource.py
+++ scripts/validate-resource.py
@@ -1,15 +1,15 @@
-import ipdb
 import os
 import sys
-from datapackage import Package
-from goodtables import validate
+import json
+from frictionless import Package
+from frictionless import validate_resource
+from pprint import pprint
 
 def resource_validation(resource_name):
-  ipdb.set_trace(context=5)
   package = Package('datapackage.json')
   resource = package.get_resource(resource_name)
-  report = validate(resource.source, schema=resource.schema.descriptor)
-  print(report)
+  report = validate_resource(resource)
+  json.dump(report, sys.stdout)
   
 if __name__ == '__main__':
     resource_validation(sys.argv[1])
