1d44abb8017ae51fba63d5289268a0c9aacba5e2
Francisco Alves <francisco.junior@cge.mg.gov.br>
2021-12-14 17:11:24 -0300

    Adiciona parse-sql.R etl-make

--- scripts/parse-sql.R
+++
@@ -1,6 +0,0 @@
-reticulate::use_condaenv("age7", required = TRUE)
-
-resource_name <- commandArgs(trailingOnly = TRUE)
-
-sql_query <- dtamg::parse_sql("datapackage.json", resource_name)
-writeLines(sql_query, glue::glue("scripts/sql/{resource_name}.sql"))
