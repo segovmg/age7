036662fbe5a1ba290d44d57bc0dcda9906f8a370
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2021-12-21 10:26:18 -0300

    Insere informações de exit-code e commit no make notify
    See transparencia-mg/age7/issues/189 e transparencia-mg/age7/issues/209

--- scripts/r/notify.R
+++ scripts/r/notify.R
@@ -2,7 +2,10 @@ outlb <- Microsoft365R::get_business_outlook()
 
 body <- blastula::render_email(here::here("scripts", "r", "notify.Rmd"))
 
+exit_code <- readLines(here::here("logs/exit-code.txt"))
+status <- ifelse(exit_code == 0, "SUCESSO", "FRACASSO")
+
 em <- outlb$create_email(body,
-                         subject = paste("Atualização age7 em:", date()),
+                         subject = glue::glue("[{status}] Atualização age7 em: {date()}"),
                          to = "catalogo.dadosabertos@cge.mg.gov.br")
 em$send()
