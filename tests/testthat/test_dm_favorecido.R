dcf0bf1aed7e92d198c2275e74f858df1336e556
Francisco Alves <francisco.junior@cge.mg.gov.br>
2021-12-21 18:51:52 -0300

    Adiciona teste dm_favorecido

---
+++ tests/testthat/test_dm_favorecido.R
@@ -0,0 +1,70 @@
+context("dm_favorecido")
+
+dm_favorecido <- fread(here::here("data/dm_favorecido.csv.gz"))
+
+#==============================================================================
+
+test_that("Anonimização CPF", {
+  
+  rules <- validate::validator(if(tp_documento == 1) grepl("\\*\\*\\*.\\d{3}.\\d{3}-\\*\\*|0", nr_documento_anonimizado))
+  report <- validate::confront(dm_favorecido, rules)
+  
+  expect_false(summary(report)[["error"]]) 
+  expect_equal(summary(report)[["fails"]], expected = 0)
+})
+
+test_that("Anonimização prêmios lotéricos", {
+
+  get_id_favorecido_premios_lotericos <- function(x) {
+    path_ft_despesa <- here::here(glue::glue("data/ft_despesa_{x}.csv.gz"))
+    path_ft_restos_pagar <- here::here(glue::glue("data/ft_restos_pagar_{x}.csv.gz"))
+    dm_unidade_orc <- fread(here::here("data/dm_unidade_orc.csv.gz"))
+    
+    ft_despesa <- fread(path_ft_despesa)[id_elemento == 494 & id_item == 2878]
+    id_favorecido_despesa <- dm_unidade_orc[ft_despesa, on = "id_unidade_orc"][cd_unidade_orc == 2041, id_favorecido]
+    
+    ft_restos_pagar <- fread(path_ft_restos_pagar)[id_elemento == 494 & id_item == 2878]
+    id_favorecido_restos_pagar <- dm_unidade_orc[ft_despesa, on = "id_unidade_orc"][cd_unidade_orc == 2041, id_favorecido]
+    
+    unique(c(id_favorecido_despesa, id_favorecido_restos_pagar))
+  }
+  
+  
+  premios_lotericos <- map(2002:2021, get_id_favorecido_premios_lotericos) |> unlist()
+    
+  dm_favorecido[
+    id_favorecido %in% premios_lotericos, 
+    is_premios_lotericos := TRUE
+  ]
+  
+  rules <- validate::validator(if(is_premios_lotericos == TRUE & tp_documento == 1) nr_documento_anonimizado == 0 & nome_anonimizado == "INFORMACAO COM RESTRICAO DE ACESSO")
+  report <- validate::confront(dm_favorecido, rules)
+  
+  expect_false(summary(report)[["error"]]) 
+  expect_equal(summary(report)[["fails"]], expected = 0)
+  
+})
+
+
+test_that("Anonimização hanseniase", {
+
+  ft_despesa_2021 <- dtamg::flatten_resource("C:\\Users/m752587/projects/age7/datapackage.json", 
+                                             "ft_despesa_2021",
+                                             c("dm_empenho_desp_2021", "dm_unidade_orc", "dm_favorecido"))
+  
+  hanseniase <- ft_despesa_2021[
+    cd_unidade_orc == 4291 & cd_unid_prog_gasto == 761,
+    unique(id_favorecido)
+  ]
+    
+  dm_favorecido[
+    id_favorecido %in% hanseniase, 
+    is_hanseniase := TRUE
+  ]
+  
+  rules <- validate::validator(if(is_hanseniase == TRUE & tp_documento == 1) nr_documento_anonimizado == 0 & nome_anonimizado == "INFORMACAO COM RESTRICAO DE ACESSO")
+  report <- validate::confront(dm_favorecido, rules)
+  
+  expect_false(summary(report)[["error"]]) 
+  expect_equal(summary(report)[["fails"]], expected = 0)
+})
