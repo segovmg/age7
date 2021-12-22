50a55ebf84e0eddd7528ecfdd3665ba9008c143e
Francisco Alves <francisco.junior@cge.mg.gov.br>
2021-12-22 17:39:46 -0300

    Atualização age7

--- all.sh
+++ all.sh
@@ -1,10 +1,14 @@
 cd $PROJECT_PATH
 source venv/Scripts/activate
+{
 git pull && \
 make parse && \
-git add . && \
-git commit -m "Atualização age7" && \
-git push origin master
+make extract && \
+# make full-extract && \
+git add . && git commit -m "Atualização age7"
+} > logs/all.txt
 echo $? > logs/exit-code.txt
 git rev-parse --short HEAD > logs/commit.txt
-make notify
+# make notify
+git add logs/notify.Rout && git commit -m "Log make notify"
+git push origin master
