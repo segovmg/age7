6e232583faf0069caafd9319b7240c4422887af7
Francisco Alves <francisco.junior@cge.mg.gov.br>
2021-12-29 08:10:52 -0300

    Executa make update && make create para publicacao em producao
    See transparencia-mg/age7/issues/231

--- run.sh
+++ run.sh
@@ -1,13 +1,4 @@
 {
-make parse && \
-make extract && \
-make ingest && \
-make data && \
-make validate && \
-make datapackage.json && \
-make test && \
-make build
+make build && \
+make create
 } > logs/run.txt
-echo $? > logs/exit-code.txt
-git rev-parse --short HEAD > logs/commit.txt
-make notify
