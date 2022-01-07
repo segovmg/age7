76eb3205e898736115e9d31c982a5b079ebc6cb3
Francisco Alves <francisco.junior@cge.mg.gov.br>
2022-01-06 21:41:47 -0300

    Atualização age7

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 {
+make extract && \
+make ingest && \
+make data && \
+make validate && \
 make datapackage.json && \
 make test && \
 make build && \
-make update
+make update && \
+git add . && git commit -m "Atualização age7"
 } > logs/run.txt
+echo $? > logs/exit-code.txt
+git rev-parse --short HEAD > logs/commit.txt
+make notify
+git add logs/notify.Rout && git commit -m "Log make notify"
+git push origin master
