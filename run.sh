0b6395e3d73f65ee333954df1a73bab57ea8437a
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2022-04-10 18:19:09 -0300

    Atualização age7

--- run.sh
+++ run.sh
@@ -1,16 +1,9 @@
-{
-make extract && \
+make clean && \
+make parse && \
+make full-extract && \
 make ingest && \
 make data && \
 make validate && \
 make datapackage.json && \
 make test && \
-make build && \
-make update && \
-git add . && git commit -m "Atualização age7"
-} > logs/run.txt
-echo $? > logs/exit-code.txt
-git rev-parse --short HEAD > logs/commit.txt
-make notify
-git add logs/notify.Rout && git commit -m "Log make notify"
-git push origin master
+make build
