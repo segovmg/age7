16585087fbe15cf894096ddfb466e9775fcda46f
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-06-14 20:05:59 -0300

    Add mail sender script

---
+++ scripts/mail_sender.py
@@ -0,0 +1,22 @@
+import sys
+import smtplib
+from email.message import EmailMessage
+
+def send_mail(EMAIL_USER, EMAIL_PASSWORD):
+  msg = EmailMessage()
+  msg['Subject'] = 'Atualizacao age7'
+  msg['From'] = EMAIL_USER
+  msg['To'] = EMAIL_USER
+  msg.set_content('''
+    Olá 
+    Segue link de validação age7
+
+    https://transparencia-mg.github.io/age7/
+    '''
+    )
+  with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+    smtp.login(EMAIL_USER, EMAIL_PASSWORD)
+    smtp.send_message(msg)
+
+if __name__ == '__main__':
+  send_mail(sys.argv[1], sys.argv[2])
